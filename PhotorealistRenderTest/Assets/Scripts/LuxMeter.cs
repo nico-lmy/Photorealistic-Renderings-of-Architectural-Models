f34aeb1c35fafd59282844a0a87e7a16c787bb4d
Nicolas Lamy <nlamy@ad.unistra.fr>
2026-06-30 10:50:36 +0200

    ajout du support du format HDR pour l'affichage des véritables valeurs de la couleur pointée

--- PhotorealistRenderTest/Assets/Scripts/LuxMeter.cs
+++ PhotorealistRenderTest/Assets/Scripts/LuxMeter.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic; 
 
 public class LuxMeter : MonoBehaviour
 {
     public Camera m_Camera; 
+    private Dictionary<int, Texture2D> readableLightmaps = new Dictionary<int, Texture2D>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,11 +27,38 @@ public class LuxMeter : MonoBehaviour
                 Renderer renderer = hit.collider.GetComponent<Renderer>();
                 if (renderer.lightmapIndex >= 0 && renderer != null) 
                 {
-                    Texture2D lightmapTex = LightmapSettings.lightmaps[renderer.lightmapIndex].lightmapColor;
-                    colorTex = lightmapTex.GetPixelBilinear(hit.lightmapCoord.x, hit.lightmapCoord.y);
+                    int lmIndex = renderer.lightmapIndex;
+                    if (!readableLightmaps.ContainsKey(lmIndex)) 
+                    {
+                        Texture2D originalLightmap = LightmapSettings.lightmaps[lmIndex].lightmapColor;
+                        readableLightmaps[lmIndex] = CreateReadableTexture(originalLightmap);
+                    }
+                    Texture2D readableTex = readableLightmaps[lmIndex];
+                    colorTex = readableTex.GetPixelBilinear(hit.lightmapCoord.x, hit.lightmapCoord.y);
                 }
                 Debug.Log("Objet touché : " + hit.collider.gameObject.name + " | Lightmap coord. : " + hit.lightmapCoord + " | Couleur : " + colorTex);
             }
         }
     }
+
+    // copie texture GPU -> CPU
+    private Texture2D CreateReadableTexture(Texture2D source)
+    {
+        RenderTexture renderTex = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.DefaultHDR, RenderTextureReadWrite.Linear);
+            
+        Graphics.Blit(source, renderTex);
+        
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = renderTex;
+        
+        Texture2D readableTex = new Texture2D(source.width, source.height, TextureFormat.RGBAHalf, false, true);
+        readableTex.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+        readableTex.Apply(); 
+        
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTex);
+        
+        return readableTex;
+    }
+
 }
