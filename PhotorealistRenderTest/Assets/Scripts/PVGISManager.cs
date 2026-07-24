c5663f3a635f5206544c2ef394f785131e135ce8
Nicolas Lamy <nlamy@ad.unistra.fr>
2026-07-24 14:01:34 +0200

    ajout du cas d'erreur quand le web request échoue car données invalides (par ex. position sur la mer)

--- PhotorealistRenderTest/Assets/Scripts/PVGISManager.cs
+++ PhotorealistRenderTest/Assets/Scripts/PVGISManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking; 
+using System.Globalization;
 
 public class PVGISManager : MonoBehaviour
 {
@@ -14,32 +15,34 @@ public class PVGISManager : MonoBehaviour
 
     IEnumerator GetPVGISData(float lat, float lon)
     {
-        string url = "https://re.jrc.ec.europa.eu/api/v5_3/tmy?lat=" + lat + "&lon=" + lon + "&outputformat=json";
+        string url = string.Format(CultureInfo.InvariantCulture, "https://re.jrc.ec.europa.eu/api/v5_3/tmy?lat={0}&lon={1}&outputformat=json", lat, lon);
 
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(url.Replace(',', '.')))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
             // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
 
-            string[] pages = url.Split('/');
-            int page = pages.Length - 1;
+            string rawJson = webRequest.downloadHandler.text;
 
             switch (webRequest.result)
             {
                 case UnityWebRequest.Result.ConnectionError:
                 case UnityWebRequest.Result.DataProcessingError:
-                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    Debug.LogError("Network Error: " + webRequest.error);
                     break;
                 case UnityWebRequest.Result.ProtocolError:
-                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    PVGISError err = null;
+                    try { err = JsonUtility.FromJson<PVGISError>(rawJson); }
+                    catch { /* JSON invalide ou vide */ }
+                    string reason = (err != null && !string.IsNullOrEmpty(err.message)) ? err.message : webRequest.error;
+                    Debug.LogWarning("PVGIS rejected the position (" + lat + ", " + lon + ") : " + reason);
                     break;
                 case UnityWebRequest.Result.Success:
-                    string rawJson = webRequest.downloadHandler.text;
                     string cleanJson = rawJson.Replace("time(UTC)", "time")
                                             .Replace("Gb(n)", "Gbn")
                                             .Replace("Gd(h)", "Gdh");
                     pvgisData = JsonUtility.FromJson<PVGISResponse>(cleanJson);
-                    Debug.Log(pages[page] + ":\nReceived all the data from TMY JSON table !\n");
+                    Debug.Log("Received all the data from TMY JSON table !\n");
                     break;
             }
         }
@@ -58,6 +61,13 @@ public class PVGISManager : MonoBehaviour
 public class PVGISResponse
 {
     public PVGISOutputs outputs;
+}
+
+[System.Serializable]
+public class PVGISError
+{
+    public string message;
+    public int status;
 }
 
 [System.Serializable]
